912c09442a2e84ac655dcc83e2c904b2010842fa
Zing <pengzl@raiyee.com>
2015-11-04 14:54:04 +0800

    refactor

---
+++ app/src/main/java/com/bingblue/group/login_logout/activity/Login.java
@@ -0,0 +1,10 @@
+package com.bingblue.group.login_logout.activity;
+
+
+import com.bingblue.group.common.activity.BaseActivity;
+
+/**
+ * Created by Zing on 15/7/14.
+ */
+public class Login extends BaseActivity {
+}
