c9220483837748ddc3d207251809ff8d5b990402
Zing <proling@126.com>
2017-01-19 22:10:05 +0800

    future：侧滑菜单重新开工

---
+++ app/src/main/java/com/zing/ablue/mainpage/activity/MainPageActivity.java
@@ -0,0 +1,15 @@
+package com.zing.ablue.mainpage.activity;
+
+import android.os.Bundle;
+
+import com.zing.ablue.R;
+import com.zing.ablue.common.activity.BaseAppCompatActivity;
+
+public class MainPageActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page_aty);
+    }
+}
---
+++ app/src/main/java/com/zing/ablue/mainpage/widget/NativeIconTextView.java
@@ -0,0 +1,39 @@
+package com.zing.ablue.mainpage.widget;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by zing on 2017/1/19.
+ */
+
+public final class NativeIconTextView extends TextView {
+    public NativeIconTextView(Context context) {
+        super(context);
+        setFontFile(context);
+    }
+
+    public NativeIconTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFontFile(context);
+    }
+
+    public NativeIconTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setFontFile(context);
+    }
+
+    public NativeIconTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setFontFile(context);
+    }
+
+    public void setFontFile(Context context) {
+        AssetManager assetManager = context.getAssets();
+        Typeface t = Typeface.createFromAsset(assetManager, "fouts/icons.ttf");
+        setTypeface(t);
+    }
+}
